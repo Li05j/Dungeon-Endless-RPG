826888078e25f8632c351b1b1e1f09f0da9cf7e6
Jason <j989li@uwaterloo.ca>
2023-09-11 21:07:16 -0400

    CON-25 supported generation of singletons

--- generate.sh
+++ generate.sh
@@ -2,6 +2,25 @@
 
 usage() { echo "Usage: $0 [insert filename]"; exit 1; }
 
+# Default flag value
+singleton=false
+
+while getopts "s" opt; do
+  case $opt in
+    s)
+      # code to handle -f flag
+      singleton=true
+      ;;
+    \?)
+      echo "Usage: $0 [-s] <filename>"
+      exit 1
+      ;;
+  esac
+done
+
+# Shift the options and arguments so that $1 refers to the filename
+shift $((OPTIND-1))
+
 if [[ "$1" == "" ]]; then
 	usage
 fi
@@ -23,8 +42,20 @@ touch $1.cpp
 
 # Print ifndef and define
 printf "#ifndef ${ALLCAPSMACRO}_${UUID}_\n#define ${ALLCAPSMACRO}_${UUID}_\n\n" >> $1.h
-# Create class, constructor and destructor.
-printf "class $CLASS_NAME {\nprivate:\n\npublic:\n\t$CLASS_NAME();\n\tvirtual ~$CLASS_NAME();\n};\n\n" >> $1.h
+
+if [ "$singleton" = false ]; then
+	# Create class, constructor and destructor.
+	printf "class $CLASS_NAME {\nprivate:\n\npublic:\n\t$CLASS_NAME();\n\tvirtual ~$CLASS_NAME();\n};\n\n" >> $1.h
+else
+	# Create singleton private constructors, destructor, and getInstance()
+	printf "class $CLASS_NAME {\nprivate:\n" >> $1.h
+	printf "\t$CLASS_NAME();\n" >> $1.h
+	printf "\tvirtual ~$CLASS_NAME();\n\n" >> $1.h
+	printf "\t$CLASS_NAME($CLASS_NAME const&) = delete; // private copy constructor\n" >> $1.h
+	printf "\t$CLASS_NAME& operator=($CLASS_NAME const&) = delete; // private assignment operator\n\n" >> $1.h
+	printf "public:\n\tstatic $CLASS_NAME& getInstance();\n};\n\n" >> $1.h
+fi
+
 # Print endif
 printf "#endif\n" >> $1.h
 
@@ -41,3 +72,9 @@ printf "#include \"./src/utils/debugUtils.h\"\n" >> $1.cpp # Include funny.h
 printf "\n" >> $1.cpp
 # Add constructor and destructor empty definition
 printf "$CLASS_NAME::$CLASS_NAME() {}\n\n$CLASS_NAME::~$CLASS_NAME() {}\n" >> $1.cpp
+
+# Add getInstance() implementation for singleton
+if [ "$singleton" = true ]; then
+	printf "\n$CLASS_NAME& $CLASS_NAME::getInstance() {\n" >> $1.cpp
+	printf "\tstatic $CLASS_NAME instance;\n\treturn instance;\n}\n" >> $1.cpp
+fi
